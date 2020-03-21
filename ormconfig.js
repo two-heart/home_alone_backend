58a717172c2e0a7576c473505ce54fe769fda38d
Daniel Schulz <daniel.schulz@incloud.de>
2020-03-21 14:26:21 +0100

    Adjusted config to database

--- ormconfig.js
+++ ormconfig.js
@@ -21,11 +21,7 @@ console.log(prefix('entity/**/*.*'));
 module.exports = {
   name: 'default',
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   entities: [prefix('**/*.entity.*')],
