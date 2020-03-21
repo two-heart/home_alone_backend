52243154df858b937d38e34ee00593bbd7fe8292
Daniel Schulz <daniel.schulz@incloud.de>
2020-03-21 16:05:52 +0100

    Adjusted userpath

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { User } from './../user/user.entity';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
@@ -6,7 +7,6 @@ import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/user.entity';
 
 @Module({
   imports: [
